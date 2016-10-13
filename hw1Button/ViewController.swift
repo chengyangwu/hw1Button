a99c605fccb9c094bb1d5713465489a417de7727
chengyangwu <chengyang.wu@gmail.com>
2016-10-13 14:56:39 +0800

    Initial Commit

---
+++ hw1Button/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw1Button
+//
+//  Created by 吳承洋 on 2016/10/13.
+//  Copyright © 2016年 edward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
